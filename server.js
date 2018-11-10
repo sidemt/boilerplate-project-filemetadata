942c13df067ecd9f945f8697e544c1a9044557c8
sidemt <25644062+sidemt@users.noreply.github.com>
2018-11-10 18:35:21 +0900

    Copy files from glitch

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 
 // require and use "multer"...
 
 var app = express();
+var upload = multer().single('upfile');
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -16,6 +18,29 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', function (req, res) {
+  if (req.file) {
+  upload(req, res, function(err){
+    if (err instanceof multer.MulterError) {
+      console.error(err);
+      res.json({"error": "An error has ocurred while uploading"});
+    } else if (err) {
+      console.error(err);
+      res.json({"error": "An error has ocurred"});
+    } else {
+      // The file was uploaded successfully
+      res.json({
+        "name": req.file.originalname,
+        "type": req.file.mimetype,
+        "size": req.file.size
+      });
+    }
+  });
+  } else {
+    res.json({"error": "file field is empty"});
+  }
 });
 
 app.listen(process.env.PORT || 3000, function () {
